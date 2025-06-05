cd6cbfd75df115f17339a5e0b5d62c4b9d5021cb
luismigueldasilvaleite <luis.leite28@escola.pr.gov.br>
2025-06-05 12:36:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'Quando acabou a segunda guerra mundial?',
+    '1945'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os principais paises da ONU?',
+    'Reino Unido, Russia, China, EUA e França'
+)
+
+criaCartao(
+    'Matemática',
+    'De quantas maneiras diferentes podemos organizar a palavra "casa", sem repetir nenhuma letra?',
+    '12 maneiras diferentes'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a função da mitocondria?',
+    'Produzir energia (ATP)'
+)
